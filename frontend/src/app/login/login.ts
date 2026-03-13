74e72ba84b966b128636d5cc2f00503720d41255
praptimeher04 <hitijamhare-spec>
2026-03-13 19:05:48 +0530

    first commit

--- frontend/src/app/login/login.ts
+++ frontend/src/app/login/login.ts
@@ -1,17 +1,33 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class LoginComponent {
+  isLoginMode = true; // Togggle between Simple Login and Simple Register
+
   constructor(private router: Router) {}
+
+  toggleMode(event?: Event) {
+    if (event) {
+      event.preventDefault();
+    }
+    this.isLoginMode = !this.isLoginMode;
+  }
 
   onLogin(event: Event) {
     event.preventDefault(); // Prevent default form submission
+    this.router.navigate(['/dashboard']);
+  }
+
+  onRegister(event: Event) {
+    event.preventDefault(); // Prevent default form submission
+    // After successful registration, nav to dashboard
     this.router.navigate(['/dashboard']);
   }
 }
